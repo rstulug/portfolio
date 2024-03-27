fe7104cd12936b84bcf855fdd26763a430baa95a
rstulug <rstulug@gmail.com>
2024-03-27 21:57:12 +0300

    initialized

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import AboutMe from "./ui/AboutMe";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+    <div className=" max-w-screen-xl font-['ui-sans-serif'] mx-auto min-h-screen">
+      <div className="flex flex-col gap-5">
+        <h1 className="flex justify-center text-5xl  pt-6">
+          Hi There <span className="wave">👋</span> I`m{" "}
+          <span className="font-semibold ml-1">Rasit Ulug</span>
+        </h1>
+        <p className="text-2xl flex justify-center">
+          Welcome My Portfolio. Take a sit and enjoy
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div className="flex justify-center mt-10">
+        <AboutMe />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
