088bf671e2e11ff1999b54abf961a916ee722238
rstulug <rstulug@gmail.com>
2024-03-29 20:30:36 +0300

    load all

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import AboutMe from "./ui/AboutMe";
 import Dashboard from "./ui/Dashboard";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { type Container, type ISourceOptions } from "@tsparticles/engine";
-// import { loadAll } from "@tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
+import { loadAll } from "@tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
 // import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
 //import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
 import Navbar from "./ui/Navbar";
@@ -10,7 +10,7 @@ import Skills from "./ui/Skills";
 import Experience from "./ui/Experience";
 import Projects from "./ui/Projects";
 import { useEffect, useMemo, useState } from "react";
-import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
+// import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
 
 function App() {
   const [init, setInit] = useState(false);
@@ -21,10 +21,10 @@ function App() {
       // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
       // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
       // starting from v2 you can add only the features you need reducing the bundle size
-      //await loadAll(engine);
+      await loadAll(engine);
       //await loadFull(engine);
       // await loadSlim(engine);
-      await loadBasic(engine);
+      // await loadBasic(engine);
     }).then(() => {
       setInit(true);
     });
