d8772a4f7ded19f3895bb7a3e90913362e177802
rstulug <rstulug@gmail.com>
2024-03-29 13:06:50 +0300

    Some adjusment for slow load

--- src/App.tsx
+++ src/App.tsx
@@ -1,135 +1,126 @@
 import AboutMe from "./ui/AboutMe";
 import Dashboard from "./ui/Dashboard";
-//import Particles, { initParticlesEngine } from "@tsparticles/react";
-//import { type Container, type ISourceOptions } from "@tsparticles/engine";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { type Container, type ISourceOptions } from "@tsparticles/engine";
 // import { loadAll } from "@tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
 // import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
-//import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
+import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
 import Navbar from "./ui/Navbar";
 import Skills from "./ui/Skills";
 import Experience from "./ui/Experience";
 import Projects from "./ui/Projects";
-//import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 // import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
 
 function App() {
-  //const [init, setInit] = useState(false);
+  const [init, setInit] = useState(false);
 
-  // // this should be run only once per application lifetime
-  // useEffect(() => {
-  //   initParticlesEngine(async (engine) => {
-  //     // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-  //     // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-  //     // starting from v2 you can add only the features you need reducing the bundle size
-  //     //await loadAll(engine);
-  //     //await loadFull(engine);
-  //     await loadSlim(engine);
-  //     //await loadBasic(engine);
-  //   }).then(() => {
-  //     setInit(true);
-  //   });
-  // }, []);
+  // this should be run only once per application lifetime
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+      // starting from v2 you can add only the features you need reducing the bundle size
+      //await loadAll(engine);
+      //await loadFull(engine);
+      await loadSlim(engine);
+      //await loadBasic(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
 
-  // const particlesLoaded = async (container?: Container): Promise<void> => {
-  //   console.log(container);
-  // };
+  const particlesLoaded = async (container?: Container): Promise<void> => {
+    console.log(container);
+  };
 
-  // const options: ISourceOptions = useMemo(
-  //   () => ({
-  //     background: {
-  //       color: {
-  //         value: "#151616",
-  //       },
-  //     },
-  //     fpsLimit: 120,
-  //     interactivity: {
-  //       events: {
-  //         onClick: {
-  //           enable: false,
-  //           mode: "push",
-  //         },
-  //         onHover: {
-  //           enable: false,
-  //           mode: "repulse",
-  //         },
-  //       },
-  //       modes: {
-  //         push: {
-  //           quantity: 4,
-  //         },
-  //         repulse: {
-  //           distance: 200,
-  //           duration: 0.4,
-  //         },
-  //       },
-  //     },
-  //     particles: {
-  //       color: {
-  //         value: "#ffffff",
-  //       },
-  //       links: {
-  //         color: "#ffffff",
-  //         distance: 150,
-  //         enable: true,
-  //         opacity: 0.5,
-  //         width: 1,
-  //       },
-  //       collisions: {
-  //         enable: false,
-  //       },
-  //       move: {
-  //         direction: "none",
-  //         enable: true,
-  //         outModes: {
-  //           default: "bounce",
-  //         },
-  //         random: false,
-  //         speed: 1,
-  //         straight: false,
-  //       },
-  //       number: {
-  //         density: {
-  //           enable: true,
-  //         },
-  //         value: 140,
-  //       },
-  //       opacity: {
-  //         value: 0.5,
-  //       },
-  //       shape: {
-  //         type: "circle",
-  //       },
-  //       size: {
-  //         value: { min: 1, max: 5 },
-  //       },
-  //     },
-  //     detectRetina: true,
-  //   }),
-  //   []
-  // );
+  const options: ISourceOptions = useMemo(
+    () => ({
+      background: {
+        color: {
+          value: "#151616",
+        },
+      },
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: false,
+            mode: "push",
+          },
+          onHover: {
+            enable: false,
+            mode: "repulse",
+          },
+        },
+        modes: {
+          push: {
+            quantity: 4,
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4,
+          },
+        },
+      },
+      particles: {
+        color: {
+          value: "#ffffff",
+        },
+        links: {
+          color: "#ffffff",
+          distance: 150,
+          enable: true,
+          opacity: 0.5,
+          width: 1,
+        },
+        collisions: {
+          enable: false,
+        },
+        move: {
+          direction: "none",
+          enable: true,
+          outModes: {
+            default: "bounce",
+          },
+          random: false,
+          speed: 1,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+          },
+          value: 140,
+        },
+        opacity: {
+          value: 0.5,
+        },
+        shape: {
+          type: "circle",
+        },
+        size: {
+          value: { min: 1, max: 5 },
+        },
+      },
+      detectRetina: true,
+    }),
+    []
+  );
 
-  // if (init) {
-  //   return (
-  //     <div className="relative ">
-  //       <Particles
-  //         id="tsparticles"
-  //         options={options}
-  //         particlesLoaded={particlesLoaded}
-  //         className="absolute top-0 left-0 w-full h-full -z-10"
-  //       />
-  //       <div className="max-w-screen-xl mx-auto text-white">
-  //         <Navbar />
-  //         <Dashboard />
-  //         <AboutMe />
-  //         <Skills />
-  //         <Experience />
-  //         <Projects />
-  //       </div>
-  //     </div>
-  //   );
-  // }
   return (
-    <div className="relative ">
+    <div
+      className="relative min-h-screen "
+      style={{ backgroundColor: init ? "" : "#151616" }}
+    >
+      {init && (
+        <Particles
+          id="tsparticles"
+          options={options}
+          particlesLoaded={particlesLoaded}
+          className="absolute top-0 left-0 w-full h-full -z-10"
+        />
+      )}
       <div className="max-w-screen-xl mx-auto text-white">
         <Navbar />
         <Dashboard />
