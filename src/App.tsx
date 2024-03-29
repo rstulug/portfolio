6004659ba9fc1c0ec4ad2fcb2f61e9006969af03
rstulug <rstulug@gmail.com>
2024-03-29 21:31:24 +0300

    slim version changed

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import Dashboard from "./ui/Dashboard";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { type Container, type ISourceOptions } from "@tsparticles/engine";
 // import { loadAll } from "@tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
-import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
-// import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
+// import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
+import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
 import Navbar from "./ui/Navbar";
 import Skills from "./ui/Skills";
 import Experience from "./ui/Experience";
@@ -22,8 +22,8 @@ function App() {
       // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
       // starting from v2 you can add only the features you need reducing the bundle size
       // await loadAll(engine);
-      await loadFull(engine);
-      // await loadSlim(engine);
+      // await loadFull(engine);
+      await loadSlim(engine);
       // await loadBasic(engine);
     }).then(() => {
       setInit(true);
